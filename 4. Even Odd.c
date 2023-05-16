824d8594c367f2267ee735bfaf133b10455f1ac9
mahmudaakternadia <116658441+mahmudaakternadia@users.noreply.github.com>
2023-05-16 22:39:44 +0600

    Update 4. Even Odd.c

--- 4. Even Odd.c
+++ 4. Even Odd.c
@@ -8,13 +8,13 @@ int main()
    scanf ("%d", &num);
 
 
-   if (num % 2 == 0)
+   if (num % 2 == 0) // % means remainder
        printf ("%d is Even Number", num);
 
     else
         printf("%d is Odd Number", num);
 
 
-    return 0;\
+    return 0;
 
 }
